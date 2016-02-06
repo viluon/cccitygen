f4d9ae93546ca03d8eec2777db6b45fc4cc8a105
Andrew Kvapil <viluon@espiv.net>
2016-02-06 16:32:33 +0100

    Fix `dofile`

--- init.lua
+++ init.lua
@@ -4,6 +4,10 @@ loaded = {}
 rawdofile = dofile
 
 function dofile( path )
+	if not fs.exists( path ) then
+		error( "No such file", 2 )
+	end
+
 	if not loaded[ path ] then
 		loaded[ path ] = { rawdofile( path ) }
 	end
