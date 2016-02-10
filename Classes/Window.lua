06c08e37dabf99dfac2478a5642438e7c9d90d21
Andrew Kvapil <viluon@espiv.net>
2016-02-10 21:43:31 +0100

    Rewrite Window Constructor
    Now this is actually somewhat usable.

--- Classes/Window.lua
+++ Classes/Window.lua
@@ -1,13 +1,20 @@
 
 dofile "Classes/Point3D.lua"
+dofile "Classes/Spacing.lua"
 
 class "Window" extends "Wall"
 {
 	align = "";
+	spacing = {};
 }
 
-function Window:Window( size, fill )
-	if type( size ) ~= "table" or not size:typeOf( Point3D ) then
-		error( "Expected Point3D, (Grid3D or Material or fill (function))", 3 )
+function Window:Window( from, to, spacing, fill )
+	if type( from ) ~= "table" or not from:typeOf( Point3D ) or type( to ) ~= "table" or not to:typeOf( Point3D ) or
+		type( spacing ) ~= "table" or not spacing:typeOf( Spacing ) then
+		error( "Expected Point3D, Spacing, (Grid3D or Material or fill (function))", 3 )
 	end
+
+	self.spacing = spacing:duplicate()
+
+	return self:Wall( from, to, fill )
 end
